338dc49b3b756afbddd6114098349431956efadc
torochi <jishun.saito.5t@stu.hosei.ac.jp>
2024-08-13 21:30:45 +0900

    敵の退場の演出とパンチのアニメーションを作成した。

---
+++ Assets/punchcontoroller.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class punchcontoroller : MonoBehaviour
+{
+    Animator animetor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animetor = this.gameObject.GetComponent<Animator>();
+        
+    }
+
+    void OnZkeypressed()
+    {
+        GetComponent<Animator>().SetTrigger("punchAnim");
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            OnZkeypressed();
+        }
+    }
+}
