56df0c0437de7ccb4a20a429cadd213ffd5c24c1
GerSaluzzo <gelsan0527@gmail.com>
2024-08-14 15:04:00 +0900

    リザルト画面のカウントアップ機能を実装

--- Assets/ShowResult.cs
+++ Assets/ShowResult.cs
@@ -1,23 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
+
 public class GameResult : MonoBehaviour
 {
     TextMeshProUGUI Score;
 
-    int result = 123; //スコア変数待ち
-    void Start()
-    {
-        Score = GetComponent<TextMeshProUGUI>();
-
-        Score.text = result.ToString();
-    }
-
-    void Update()
+    //int result = gamedirector.n; //スコア変数待ち
+    int result = 18;
+    int i = 0;
+    
+  private void Start()
     {
         
+        {
+            if (i <= result)
+            {
+                InvokeRepeating(nameof(RepeatMsg), 0.2f, 0.1f);
+  
+            }
+
+            
+        }
+
+       
+            
+        }
+
+        void RepeatMsg()
+        {
+        if (i <= result)
+        {
+            Score = GetComponent<TextMeshProUGUI>();
+
+            Score.text = i.ToString() + "人抜き！";
+
+            i++;
+
+        }
+        else
+            {
+                CancelInvoke();
+            }
+
+            
+
     }
 }
